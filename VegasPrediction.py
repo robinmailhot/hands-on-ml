a0b0788fbb56ff09943147f06918005dc1de39db
Robin Mailhot <robin.mailhot@usherbrooke.ca>
2020-11-16 19:45:52 -0500

    tested search grid and random forest

--- VegasPrediction.py
+++ VegasPrediction.py
@@ -9,7 +9,17 @@ from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.metrics import mean_squared_error
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestRegressor
 
+#-----------------------------------------------------------
+# This script is a play ground for me to learn to use
+# sklearn on real data. I realized late in the exercise
+# that the data is atrocious for machine learning and there
+# isnt much that can be done to make good predictions but
+# still continued to explore it !
+#-----------------------------------------------------------
 vegas = import_vegas_data()
 
 ## Test splits
@@ -57,9 +67,30 @@ predictions = lr.predict(vegas_test_input)
 linear_mse = mean_squared_error(predictions,vegas_test_output)
 print('Error with linear regression : ', np.sqrt(linear_mse))
 
-# Trying with DecisionTree
-DecisionTree = DecisionTreeRegressor()
-DecisionTree.fit(vegas_train_input,vegas_train_output)
-predictions = DecisionTree.predict(vegas_test_input)
+# Trying with dt
+dt = DecisionTreeRegressor()
+dt.fit(vegas_train_input, vegas_train_output)
+predictions = dt.predict(vegas_test_input)
 DecisionTree_mse = mean_squared_error(predictions,vegas_test_output)
 print('Error Decision Tree : ', np.sqrt(DecisionTree_mse))
+
+# TESTING ON CROSS VALIDATION SETS
+scores = cross_val_score(dt, vegas_train_input, vegas_train_output, scoring='neg_mean_squared_error', cv=8)
+dt_scores = np.sqrt(-scores)
+
+scores = cross_val_score(lr, vegas_train_input, vegas_train_output, scoring='neg_mean_squared_error', cv=8)
+lr_scores = np.sqrt(-scores)
+
+#  GRID SEARCH FOR RANDOM FOREST REGRESSOR
+parameters_grid = [{'n_estimators':[5,  10, 20], 'max_features':[10,15,20]}]
+
+forrest_reg = RandomForestRegressor()
+grid_search = GridSearchCV(forrest_reg,parameters_grid, scoring='neg_mean_squared_error',return_train_score=True)
+grid_search.fit(vegas_test_input,vegas_test_output)
+
+final_model = grid_search.best_estimator_
+scores = cross_val_score(final_model, vegas_train_input, vegas_train_output, scoring='neg_mean_squared_error', cv=8)
+final_scores = np.sqrt(-scores)
+
+# we can conclude that linear regression still does better than a fine tuned random forest.
+# the goal of this not to din the best results possible but to learn to use sklearn, we can say goal accomplished
