7ccba8d5cbeed9d715eab60c76be391aef304b7a
Robin Mailhot <robin.mailhot@usherbrooke.ca>
2020-12-31 21:51:07 -0500

    done with the mnist test branch

--- Classification/mnist_classification.py
+++ Classification/mnist_classification.py
@@ -1,17 +1,13 @@
 from sklearn.datasets import fetch_openml
-import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as  np
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import cross_val_score
 
 
-
 mnist = fetch_openml('mnist_784', version=1)
 Data, Target = mnist['data'], mnist['target']
 Target = Target.astype(np.uint8)
-
-
 first_number = Data[0]
 first_number_reshaped = first_number.reshape(28,28)
 plt.imshow(first_number_reshaped)
@@ -20,7 +16,7 @@ plt.show()
 
 X_train, X_test, Y_train, Y_test = Data[:60000], Data[60000:], Target[:60000], Target[60000:]
 
-knn = KNeighborsClassifier(n_jobs=10)
+knn = KNeighborsClassifier(n_jobs=4)
 knn.fit(X_train,Y_train)
 result = cross_val_score(knn, X_train, Y_train,cv=3,scoring='accuracy')
 #  This will do it to for a small test of how the API works for model fitting and data loading with sklearn
