521d6d88a3b3eeed2ea00fd400bc332496338646
Robin Mailhot <robin.mailhot@usherbrooke.ca>
2020-12-31 21:43:01 -0500

    minor changes, too slow to load mnist form sklearn'

--- Classification/mnist_classification.py
+++ Classification/mnist_classification.py
@@ -23,4 +23,4 @@ X_train, X_test, Y_train, Y_test = Data[:60000], Data[60000:], Target[:60000], T
 knn = KNeighborsClassifier(n_jobs=10)
 knn.fit(X_train,Y_train)
 result = cross_val_score(knn, X_train, Y_train,cv=3,scoring='accuracy')
-print('moma')
+#  This will do it to for a small test of how the API works for model fitting and data loading with sklearn
